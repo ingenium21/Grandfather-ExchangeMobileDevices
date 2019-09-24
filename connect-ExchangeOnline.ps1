00dfc42e56c53936629cdc956c7d7a4da651d915
Renato Regalado <renato.regalado@gmail.com>
2019-09-24 14:50:52 -0500

    Create connect-ExchangeOnline.ps1

---
+++ connect-ExchangeOnline.ps1
@@ -0,0 +1,7 @@
+# Purpose of this script is to connect to Exchange Online
+
+$UserCredential = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session -DisableNameChecking
